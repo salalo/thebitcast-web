ba45da65be59bae2b2f3494508969863eb0d98d3
Wojciech Sala <wojteksala190@gmail.com>
2019-02-23 00:03:16 +0100

    player hovers for volume and multiplier

--- client/src/components/Player.vue
+++ client/src/components/Player.vue
@@ -35,14 +35,32 @@
 
 		<div class="row player__options">
 
-			<font-awesome-icon :icon="['fas', 'volume-up']" class="icon icon__options alt"/>
+			<font-awesome-icon :icon="['fas', 'volume-up']" class="icon icon__options player__options-volume alt"/>
 
-			<font-awesome-icon :icon="['fas', 'tachometer-alt']" class="icon icon__options alt"/>
+			<font-awesome-icon :icon="['fas', 'tachometer-alt']" class="icon icon__options player__options-mult alt"/>
 
 			<font-awesome-icon
 				:icon="[prefab, 'bookmark']"
 				v-on:click="addToBookmarks()"
-				class="icon icon__options alt"
+				class="icon icon__options player__options-bookmarks alt"
+			/>
+
+			<div class="player__options-multL mult">
+				<span>0.25</span>
+				<span>0.5</span>
+				<span>0.75</span>
+			</div>
+			<div class="player__options-multR mult">
+				<span>1.25</span>
+				<span>1.5</span>
+				<span>1.75</span>
+			</div>
+
+			<q-slider
+				:min="0"
+				:max="100"
+				class="player__options-slider"
+				value=100
 			/>
 		</div>
 	</div>
@@ -96,9 +114,66 @@ export default {
 		width: 330px;
 		display: inline-block;
 
-		:first-child { float: left; }
-		:nth-child(3) { float: middle; }
-		:last-child { float: right; }
+		&-volume {
+			float: left;
+			&:hover {
+				~.player__options-slider {
+					opacity: 1;
+					@include transition(0s, opacity .5s);
+				}
+			}
+		}
+
+		&-slider {
+			opacity: 0;
+			@include transition(.5s, opacity .7s);
+			&:hover { opacity: 1; }
+			
+			width: 100px;
+			color: $main;
+			margin: -25px 0 0 25px;
+		}
+
+
+		&-mult {
+			&:hover {
+				~.mult {
+					opacity: 1;
+					@include transition(0s, opacity .5s);
+				}
+			}
+		}
+		&-multL {
+			margin-left: 30px;
+			float: left;
+		}
+		&-multR {
+			margin-right: 30px;
+			float: right;
+		}
+		.mult {
+			@include transition(.5s, opacity .7s);
+			opacity: 0;
+			&:hover { opacity: 1; }
+
+			display: inline-block;
+			font-size: 12px;
+			font-weight: 300;
+			margin-top: -1px;
+
+			span {
+				margin: 0 5px 0 5px;
+				&:hover {
+					cursor: pointer;
+				}
+			}
+			background-color: $lighter-main;
+			color: $white;
+			border-radius: 5px;
+			padding: 2px;
+		}
+
+		&-bookmarks { float: right; }
 	}
 }
 .time-sets {
