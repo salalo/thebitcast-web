3f44369471d487e4654714c81b872316d39e01d8
akad <wojteksala190@gmail.com>
2020-04-08 00:27:11 +0200

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
-# thebitcast-web
+# What's The Bitcast?
+
+The Bitcast is a platform that tries to solve quality problem in podcasting niche. Also we tried to allow podcasters to earn fairer money for they work and let them focus on their passion by providing great platform and monetization system.
+
+<br>
+
+### <center>Please read this before continuing. </center>
+
+#### I've decided to share process and our struggles on the project as a team in a nutshell so there you go.
+
+#### We later on divided the project in different repos so the newest server part is a private repo, but you can see [mobile version](https://github.com/WojciechSala/thebitcast-mobile) which we worked on as the last.
+
+#### The actual repository of web version is abandoned as we decided to go to the market with mobile app first. The mobile app would probably be used for bigger group of people, so until that we decided to change our priorities.
+
+---
+
+# Stack we used
+
+### Front-end
+
+- Vue.js
+- Vuex for state management
+- Axios and fetch for API requests
+- SCSS preprocessor for readable CSS
+- Vuetify for material design components and icons
+
+### Back-end
+
+- Node.js with express
+- MongoDB at first but decided to switch to MySQL as it was a better solution for our case.
+- Passport.js for auths
+- Bcrypt for hashing passwords
+- Joi for data validation
+
+### Design
+
+- Adobe XD with Figma for design and prototyping
+- Illustrator with Photoshop for graphics
+- Material design style guides
+
+### Team management
+
+- Notion with trello for project management
+- Github for developers
+- Google drive for file sharing
+
+---
+
+# Challenges we faced
+
+- Creating well thought design both on UI and UX side.
+- The RWD was pretty hard, especially because we didn't made it "mobile first" at the beginning.
+- Scalability played the first role to optimize the platform enough, choosing the right DB setup and backend stack took as a lot of learning.
+- At some point we decided to use AWS to host our app which again gave us a lot of problems to face as well as knowledge that came with that.
+- We had to learn a lot on JWT, authentication and authorization flows in general and different libraries.
+- Managing the team and workflow to stay efficient as possible.
+
+---
+
+# Mistakes we made
+
+- The biggest mistake was we didn't think everything at the beginning but started developing. We lost a lot of time changing the stack we used.
+- We didn't code mobile first.
+- We should have started from mobile app.
+- Finally we didn't look for help outside in form of investors.
+
+---
+
+# Decision we determined
+
+At some point where we lost our motivation we decided to pause the project and get a fresh start in the future with more experience.
+
+#### Still even though we stopped we learned A LOT both technical and soft skills. We would never say that we lost our time doing this project.
+
+---
+
+# Installation
+
+##### At that point the project is not hosted anywhere but you can run it locally.
+
+1. Clone the repo `git clone https://github.com/WojciechSala/thebitcast-web.git`
+2. Install all libraries in package.json directories (both client and server) `npm install`
+3. Run the client and backend server by `npm run dev`
+4. To see the other part of the app after login you will have to choose:
+   - EASY way: go to client/src/components/Home/Desktop.vue and set v-if="true" to work around and pass the authentication.
+   - HARD way: Set up MySQL db and keys.js for authentication and fully working app.
+
+---
+
+# Contribution
+
+We don't expect any contributions, the project itself was meant to be commercialized and we still believe in reviving it at some point.
+
+---
+
+# Licensing
+
+The license may at some point be custom made and we encourage to check it before any actions.
--- server/src/actions/user.js
+++ server/src/actions/user.js
@@ -1,10 +1,9 @@
-import db from "../config/db.js";
-import dateTime from "node-datetime";
-import bcrypt from "bcrypt";
+import db from '../config/db.js';
+import dateTime from 'node-datetime';
 
 function getActualTime() {
   const dt = dateTime.create();
-  return dt.format("Y-m-d H:M:S");
+  return dt.format('Y-m-d H:M:S');
 }
 
 export default {
@@ -17,67 +16,66 @@ export default {
         email +
         '"';
 
-      let res = await db.query(sql)
-      if (!res) resolve("dbError");
+      let res = await db.query(sql);
+      if (!res) resolve('dbError');
       if (res.length > 0) resolve(true);
       resolve(false);
-
     });
   },
 
   getUserByUnique(unique, accountType) {
-    return new Promise(async(resolve, reject)=>{
-      let sql = "SELECT * FROM Users WHERE ";
+    return new Promise(async (resolve, reject) => {
+      let sql = 'SELECT * FROM Users WHERE ';
 
       switch (accountType) {
-        case "local":
-          sql += "email";
+        case 'local':
+          sql += 'email';
           break;
-        case "google":
-          sql += "google_ID";
+        case 'google':
+          sql += 'google_ID';
           break;
-        case "twitter":
-          sql += "twitter_ID";
+        case 'twitter':
+          sql += 'twitter_ID';
           break;
-        case "facebook":
-          sql += "facebook_ID";
+        case 'facebook':
+          sql += 'facebook_ID';
           break;
       }
       sql += '="' + unique + '"';
-  
-      let res = await db.query(sql)
-      if(res === []) resolve(undefined)
-      else if(!res) resolve(false)
-      resolve(res[0])
-    })
+
+      let res = await db.query(sql);
+      if (res === []) resolve(undefined);
+      else if (!res) resolve(false);
+      resolve(res[0]);
+    });
   },
 
   addUser(USER, accountType) {
-    return new Promise(async (resolve, reject)=>{ 
-      let facebook_ID = "NULL";
-      let google_ID = "NULL";
-      let twitter_ID = "NULL";
+    return new Promise(async (resolve, reject) => {
+      let facebook_ID = 'NULL';
+      let google_ID = 'NULL';
+      let twitter_ID = 'NULL';
       let nick = USER.displayName;
       let sql;
 
       switch (accountType) {
-        case "facebook":
-          nick = USER.name.givenName + " " + USER.name.familyName;
+        case 'facebook':
+          nick = USER.name.givenName + ' ' + USER.name.familyName;
           facebook_ID = '"' + USER.id + '"';
           break;
-        case "twitter":
+        case 'twitter':
           twitter_ID = '"' + USER.id + '"';
           break;
-        case "google":
+        case 'google':
           google_ID = '"' + USER.id + '"';
           break;
       }
 
-      if (accountType === "local") {
+      if (accountType === 'local') {
         sql =
-          "INSERT INTO Users (" +
-          "ID, nick, email, password, register_date, last_login, avatar_href, google_ID, facebook_ID, twitter_ID," +
-          "facebook_link, twitter_link, instagram_link, gender, description, activated, premium, banned, region_ID, language_ID, email_notifications, push_notifications) VALUES" +
+          'INSERT INTO Users (' +
+          'ID, nick, email, password, register_date, last_login, avatar_href, google_ID, facebook_ID, twitter_ID,' +
+          'facebook_link, twitter_link, instagram_link, gender, description, activated, premium, banned, region_ID, language_ID, email_notifications, push_notifications) VALUES' +
           '(NULL, "' +
           USER.nick +
           '", "' +
@@ -89,12 +87,12 @@ export default {
           '", "' +
           getActualTime() +
           '", NULL, ' +
-          "NULL, NULL, NULL , NULL, NULL, NULL, NULL, NULL, 0, 0, 0,0,0,0,0)";
+          'NULL, NULL, NULL , NULL, NULL, NULL, NULL, NULL, 0, 0, 0,0,0,0,0)';
       } else {
         sql =
-          "INSERT INTO Users (" +
-          "ID, nick, email, password, register_date, last_login, avatar_href, google_ID, facebook_ID, twitter_ID," +
-          "facebook_link, twitter_link, instagram_link, gender, description, activated, premium, banned, region_ID, language_ID, email_notifications, push_notifications) VALUES" +
+          'INSERT INTO Users (' +
+          'ID, nick, email, password, register_date, last_login, avatar_href, google_ID, facebook_ID, twitter_ID,' +
+          'facebook_link, twitter_link, instagram_link, gender, description, activated, premium, banned, region_ID, language_ID, email_notifications, push_notifications) VALUES' +
           '(NULL, "' +
           nick +
           '", "' +
@@ -105,38 +103,36 @@ export default {
           getActualTime() +
           '", NULL, ' +
           google_ID +
-          ", " +
+          ', ' +
           facebook_ID +
-          ", " +
+          ', ' +
           twitter_ID +
-          ", NULL, NULL, NULL, NULL, NULL, 0, 0, 0,0,0,0,0)";
+          ', NULL, NULL, NULL, NULL, NULL, 0, 0, 0,0,0,0,0)';
       }
-      
-      if(!await db.query(sql)) resolve(false)
-      resolve(true)
-    })
-    
+
+      if (!(await db.query(sql))) resolve(false);
+      resolve(true);
+    });
   },
 
   getUserByID(ID) {
-    return new Promise(async(resolve, reject)=>{
+    return new Promise(async (resolve, reject) => {
+      let sql = 'SELECT * FROM Users WHERE ID=' + ID;
 
-      let sql = "SELECT * FROM Users WHERE ID=" + ID;
-
-      let res = await db.query(sql)
-      if(!res)resolve(false)
-      delete res[0].password
-      resolve(res[0])
-    })
+      let res = await db.query(sql);
+      if (!res) resolve(false);
+      delete res[0].password;
+      resolve(res[0]);
+    });
   },
 
   updateLastLogin(ID) {
-    return new Promise(async(resolve, reject)=>{
+    return new Promise(async (resolve, reject) => {
       let sql =
-      'UPDATE Users SET last_login="' + getActualTime() + '" WHERE ID=' + ID;
+        'UPDATE Users SET last_login="' + getActualTime() + '" WHERE ID=' + ID;
 
-      if(!await db.query(sql)) resolve(false)
-      resolve(true)
-    })
-  }
+      if (!(await db.query(sql))) resolve(false);
+      resolve(true);
+    });
+  },
 };
